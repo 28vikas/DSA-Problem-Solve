59393956f3ddf04bfc5814ea509cfc80d819284c
28vikas <63vikas@gmail.com>
2023-06-07 22:21:07 +0530

    count string

---
+++ Count the Number of Consistent Strings/1684._Count_the_Number_of_Consistent_Strings.js
@@ -0,0 +1,23 @@
+
+
+let allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+
+function xyz(allowed){
+    let result = [];
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i]
+  
+      for (let j = 0; j < word.length; j++) {
+        const char = word[j];
+  
+        if (!allowed.includes(char)) break;
+        else if (j === word.length - 1) result.push(word)
+      }
+    }
+  
+    return result.length;
+}
+
+
+console.log(xyz(allowed))
